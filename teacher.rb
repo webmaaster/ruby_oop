4d5e47e210d3c27dbb8f6de2a8bed7b0e0f6fb81
webmaaster <wecodewetest@gmail.com>
2020-12-29 15:46:59 -0500

    Completed Object Oriented Programming

--- teacher.rb
+++ teacher.rb
@@ -1,18 +1,34 @@
 class Teacher
-    @first_name 
-    @last_name
-    @username
-    @email
-    @username 
-    @password
+    attr_accessor :first_name, :last_name, :username, :email, :password
+
+    def initialize(firstname, lastname, username, email, password)
+        @first_name = firstname
+        @last_name = lastname
+        @username = username
+        @email =email
+        @password = password
+
+    end 
+
+    # def first_name=(name)
+    #     @first_name = name 
+    # end 
+
+    # def first_name
+    #     @first_name 
+    # end 
 
     def to_s
-        "First name: #{@first_name}"
+        "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, Email: #{@email}, Password: #{@password}"
     end 
 
 end 
 
-masud = Teacher.new 
+masud = Teacher.new("Masudur Rahman", "Chawdhury", "masud", 
+                   "masud@gmail.com", "admin1")
+babu = Teacher.new("Babu", "Rahman", "babu", 
+                   "babu@gmail.com", "admin2")
+
+
 puts masud
-masud.first_name = "Masudur Rahman" 
-puts masud 
+puts babu 
